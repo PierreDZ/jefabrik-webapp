c7adc3e5253a5363da30fd6673dd8a9f800af243
Pierre DZ <pierre.dhontzeimert@gmail.com>
2021-09-01 16:39:39 +0200

    Adding auth0 the the app with secure routes

--- jefabrik-webapp/src/router/index.js
+++ jefabrik-webapp/src/router/index.js
@@ -5,6 +5,7 @@ import Tracking from '../views/Tracking.vue'
 import Integration from '../views/Integration.vue'
 import Personalization from '../views/Personalization.vue'
 import Login from '../views/Login.vue'
+import { authGuard } from "../auth/authGuard"
 
 
 Vue.use(VueRouter)
@@ -18,22 +19,26 @@ const routes = [
   {
     path: '/Dashboard',
     name: 'Dashboard',
-    component: Dashboard
+    component: Dashboard,
+    beforeEnter: authGuard
   },
   {
     path: '/Tracking-infos',
     name: 'Tracking',
-    component: Tracking
+    component: Tracking,
+    beforeEnter: authGuard
   },
   {
     path: '/Integration',
     name: 'Integration',
-    component: Integration
+    component: Integration,
+    beforeEnter: authGuard
   },
   {
     path: '/Personalization',
     name: 'Personalization',
-    component: Personalization
+    component: Personalization,
+    beforeEnter: authGuard
   }
 ]
 
--- jefabrik-webapp/src/views/Login.vue
+++ jefabrik-webapp/src/views/Login.vue
@@ -1,110 +1,95 @@
 <template>
-    <div class="login">
-      <section class="background-img">
-          <h1>Jefabrik Analytics</h1>
-      </section>
-      <section class="log">
-          <div class="inner-log">
-              <p>Let's get started</p>
-              <form @submit="checkForm">
-                  <label for="email">Email adress</label>
-                  <input type="mail" name="email">
-                  <label for="password">Password</label>
-                  <input type="password" name="password">
-                  <button type="submit" @click="loginIn">Log In</button>
-              </form>
-          </div>
-      </section>
-    </div>
+  <div class="login">
+    <section class="background-img">
+      <h1>Jefabrik Analytics</h1>
+    </section>
+    <section class="log">
+      <div class="inner-log">
+        <p>Let's get started</p>
+        <div v-if="!$auth.loading">
+          <!-- show login when not authenticated -->
+          <button class="btn" v-if="!$auth.isAuthenticated" @click="login">
+            Connect to the App
+          </button>
+          <!-- show logout when authenticated -->
+          <button class="btn" v-if="$auth.isAuthenticated" @click="logout">
+            Log out
+          </button>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'Login',
-    methods: {
-        checkForm (e) {
-        e.preventDefault();
-        },
-        loginIn () {
-        this.$router.push('Dashboard');
-        }
-  }
-}
+  name: "Login",
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin,
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
- .login {
-    display: grid;
-    height: 100%;
-    grid-template-columns: repeat(2, 1fr);
+.login {
+  display: grid;
+  height: 100%;
+  grid-template-columns: repeat(2, 1fr);
 
-    .background-img {
-        display: flex;
-        justify-content: center;
-        background-color: rgba(38, 206, 131, 1);
+  .background-img {
+    display: flex;
+    justify-content: center;
+    background-color: rgba(38, 206, 131, 1);
 
-        h1 {
-            margin-top: 110px;
-            color: white;
-            font-size: 53px;
-        }
+    h1 {
+      margin-top: 110px;
+      color: white;
+      font-size: 53px;
     }
+  }
 
-    .log {
-        width: 80%;
-        height: 80%;
-        justify-self: center;
-        align-self: center;
-        text-align: left;
+  .log {
+    width: 80%;
+    height: 80%;
+    justify-self: center;
+    align-self: center;
+    text-align: left;
 
-        .inner-log {
-            display: flex;
-            flex-direction: column;
+    .inner-log {
+      display: flex;
+      flex-direction: column;
 
-            p {
-                margin: 50px 0;
-                color: black;
-                font-size: 36px;
-                font-weight: bold;
-            }
+      p {
+        margin: 50px 0;
+        color: black;
+        font-size: 36px;
+        font-weight: bold;
+      }
 
-            form {
-                display: flex;
-                flex-direction: column;
+      .btn {
+        cursor: pointer;
+        height: 64px;
+        width: 100%;
+        border-radius: 10px;
+        border: 1px solid #26ce83;
+        background-color: transparent;
+        color: #26ce83;
+        font-size: 18px;
+        transition: all 0.3s ease-in-out;
 
-                input {
-                    height: 64px;
-                    border-radius: 10px;
-                    border: 1px solid #C8C8C8;
-                    margin-bottom: 50px;
-                    outline: none;
-                    font-size: 18px;
-                    padding: 0 20px;
-
-                    &:focus,
-                    &:hover {
-                        border-color: #26ce83;
-                    }
-                }
-
-                button {
-                    cursor: pointer;
-                    height: 64px;
-                    border-radius: 10px;
-                    border: 1px solid #26ce83;
-                    background-color: transparent;
-                    color: #26ce83;
-                    font-size: 18px;
-                    transition: all 0.3s ease-in-out;
-
-                    &:hover {
-                        background-color: #26ce83;
-                        color: white;
-                    }
-                }
-            }
+        &:hover {
+          background-color: #26ce83;
+          color: white;
         }
+      }
     }
+  }
 }
 </style>
